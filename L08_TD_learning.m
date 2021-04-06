9a3092bfe5a09e025722590d05ce8fbd02601a41
YuangCheng <yuang_cheng@pku.edu.cn>
2021-04-07 00:53:20 +0800

    Update L08_TD_learning.m

--- L08_TD_learning.m
+++ L08_TD_learning.m
@@ -1,6 +1,10 @@
+%% Temporal Difference Learning (Sarsa)
 clear
 tic
+% value_table for 6 states
 Value=zeros(6);
+% -1 means impossible transition
+% state 6 is the destination
 rewardtable =...
     [-1, -1, -1, -1, 0, -1;
     -1, -1, -1, 0, -1, 100;
@@ -8,29 +12,37 @@ rewardtable =...
     -1, 0, 0, -1, 0, -1;
     0, -1, -1, 0, -1, 100;
     -1, 0, -1, -1, 0, 100];
-gamma=0.1;
-alpha=0.8;
-eps=0.1;
+gamma=0.1;% temporal discounting
+alpha=0.8;% learning_rate
+eps=0.1;% epsilon
 iter_num=100000;
 all=1:6;
-for i=1:iter_num % 训练iter_num次
-    state=randi(6);% 每次随机放在一个位置
-    while state~=6 % 如果没到终点，走到下一个位置，更新这个位置的value
-        % eps greedy
+
+% train
+for i=1:iter_num % train iter_num times
+    state=randi(6);% randomly put it at the first iteration
+    while state~=6 % loop until destination
+        % epsilon-greedy
         toss=binornd(1,eps);
-        if toss==1 % exploration
+        % exploration
+        % randomly choose the action
+        if toss==1 
             temp=all(rewardtable(state,:)>=0);
             action=temp(randperm(length(temp),1));
-        else
+        % exploitation
+        % choose the action with the greatest reward
+        else 
             tempmax=max(Value(state,:));
             tempindex=all((Value(state,:)==tempmax)&(rewardtable(state,:)>=0));
             action=tempindex(randperm(length(tempindex),1));
         end
-        immediate_reward=rewardtable(state,action);% 即时reward
-        next_state=action;% 下一个位置
-        % eps greedy
+        immediate_reward=rewardtable(state,action);
+        next_state=action;
+        % epsilon-greedy
         toss=binornd(1,eps);
-        if toss==1 % exploration
+        % exploration
+        % randomly choose the next action
+        if toss==1
             temp=all(rewardtable(next_state,:)>=0);
             next_action=temp(randperm(length(temp),1));
         else
@@ -38,27 +50,31 @@ for i=1:iter_num % 训练iter_num次
             tempindex=all((Value(next_state,:)==tempmax)&(rewardtable(next_state,:)>=0));
             next_action=tempindex(randperm(length(tempindex),1));
         end
+        % calculate the prediction error
         prediction_error=immediate_reward+gamma*Value(next_state,next_action)-Value(state,action);
+        % update the value table
         Value(state,action)=Value(state,action)+alpha*prediction_error;
         state = next_state;
         action = next_action;
     end
 end
-for j=1:10
-    state=randi(3);
-    count=0;% 走了多少步
-    path(j).statechain=[];% 记录怎么走的
+% test
+for j= 1:10
+    state=randi(3);% the starting point can only be at state 1-3
+    count=0;% the number of steps taken
+    path(j).statechain=[];% record the path
     path(j).statechain=[path(j).statechain state];
     while state~=6
-        if count>5 % 5步没走出来就算失败
+        if count > 5 % if more than 5 steps, then failure
             path(j).statechain=[];
             break
         end
+        % greedy
         tempmax=max(Value(state,:));
         tempindex=all(Value(state,:)==tempmax);
         next_state=tempindex(randperm(length(tempindex),1));
-        path(j).statechain=[path(j).statechain next_state];
-        state=next_state;
+        path(j).statechain=[path(j).statechain next_state]; % record the path
+        state=next_state; % update the state
         count=count+1;
         path(j).count=count;
     end
