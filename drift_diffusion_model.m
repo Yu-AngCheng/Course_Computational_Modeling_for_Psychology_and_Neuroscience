531b3a23bbe1780c211f7bde3ea99480ade9454b
YuangCheng <yuang_cheng@pku.edu.cn>
2021-04-07 02:26:56 +0800

    Update drift_diffusion_model.m

--- drift_diffusion_model.m
+++ drift_diffusion_model.m
@@ -1,16 +1,17 @@
 %% DDM
 clear
-a=1; % upper bound
-z=0.5;% percentage of starting point
-e=0;% end
+a = 1; % upper bound
+z = 0.5;% percentage of starting point
+e = 0;% end
 drift_rate=0.5;
 steps=0.001;
-s=1;% standard error of one step
-current=a*z;% real starting point
-time=0;
-t(1)=time;
-evidence(1)=current;
-n=2;% just for index
+s = 1;% standard error of one step
+current = a*z;% real starting point
+time = 0;
+
+t(1) = time;
+evidence(1) = current;
+n = 2;% just for index
 while (current<a)&&(current>e)
     time=time+steps;
     current=current+normrnd(drift_rate*steps,sqrt(s*s*steps));
