9f5237ae54ba360857a6b28eeed47c17dd58304a
YuangCheng <yuang_cheng@pku.edu.cn>
2021-04-06 22:01:54 +0800

    Update L08_SPRT.m

--- L08_SPRT.m
+++ L08_SPRT.m
@@ -1,13 +1,16 @@
-% Sequential Probability Ratio Test
-miu1=4;
-miu2=2;
-sigma1=3;
-sigma2=3;
-% H0 x from population 1
-% H1 x from population 2
+%% Sequential Probability Ratio Test
+% parameters for 2 normal distributions
+miu_1=4;
+miu_2=2;
+sigma_1=3;
+sigma_2=3;
+
+% H0: sample x is from population 1
+% H1: sample x is from population 2
 log_prior_odds=log(1);
 log_posterior_odds=log_prior_odds;
 for i=1:200000
+    % set the criteria
     if(log_posterior_odds>30)
         fprintf("H1 is correct\n");
         break;
@@ -15,7 +18,10 @@ for i=1:200000
         fprintf("H0 is correct\n");
         break;
     end
-    x=normrnd(miu2,sigma2);
-    log_likelihood_ratio=log(normpdf(x,miu2,sigma2)/normpdf(x,miu1,sigma1));
+    % get a sample
+    x=normrnd(miu_2,sigma_2);
+    % calculate the likelihood
+    log_likelihood_ratio=log(normpdf(x,miu_2,sigma_2)/normpdf(x,miu_1,sigma_1));
+    % update the posterior
     log_posterior_odds=log_posterior_odds+log_likelihood_ratio;
 end
