23f890cce1827005daa466b05506b52e9ec08124
YuangCheng <yuang_cheng@pku.edu.cn>
2021-04-24 14:19:59 +0800

    Revert "update"
    This reverts commit 9a04e77e5f8abe8d402a7d96729feb76ce4f59e2.

--- homework/homework 1/code/question2/paraestimation.m
+++
@@ -1,9 +0,0 @@
-function paramsEst = paraestimation(data)
-LB = [-Inf, -Inf];
-UB = [Inf, Inf];
-x0 = [45, 0.1];
-intensity_level=1:100;
-paramsEst = fminsearchbnd(@(params)mll(params, intensity_level, data), x0, LB, UB);
-
-end
-
